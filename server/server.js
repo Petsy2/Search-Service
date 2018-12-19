5da952e667030c0cbb6fc10f37177b3d7ff07e58
Sean <meyersean.c@gmail.com>
2018-12-19 11:01:57 -0600

    (doc) Redo on boiler plate

--- server/server.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const app = express();
-const port = 1337;
-
-app.use(express.static("/"));
-
-app.listen(port, `related items server is listening on port: ${port}`);
