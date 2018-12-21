0856b27174c000f3b5ebf3780103ac5889a6ffb4
Sean <meyersean.c@gmail.com>
2018-12-21 09:27:10 -0600

    (feat) Seperate test files for ease of use

---
+++ test/service_tests.js
@@ -0,0 +1,7 @@
+const assert = require("assert");
+const dbTESTDB = require("./DBtest/test_db");
+const dbHELPERS = require("./DBtest/test_helpers");
+const dbCREATE = require("./DBtest/create_test");
+const dbDELETE = require("./DBtest/delete_test");
+const dbREAD = require("./DBtest/read_test");
+const dbUPDATE = require("./DBtest/update_test");
