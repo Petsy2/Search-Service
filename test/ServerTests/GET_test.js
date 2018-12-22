a4d6f09c83f99adfe1e1d736e50689a697c20850
Sean <meyersean.c@gmail.com>
2018-12-22 11:00:29 -0600

    (feat) Got first server test to fail, good start

--- test/ServerTests/GET_test.js
+++ test/ServerTests/GET_test.js
@@ -2,21 +2,21 @@ const should = require("chai").should();
 const express = require("express");
 const axios = require("axios");
 const url = "http://localhost:1337/api/recommends";
-const server = require("../../Server/server.js");
 
 describe("GET one", function() {
-  it("should route a get request for a pet_id", function() {
-    const pet = { pet_id: 1111 };
-    console.log(pet);
-
+  const mockReq = params => {
     axios
-      .get(url, {
-        params: {
-          pet_id: 1111
-        }
-      })
-      .then(response => response.species.should.equal("Tiger"))
+      .get(params)
+      .then(response => response)
+      .catch(err => {});
+  };
+
+  it("should route a get request for a pet_id", function() {
+    tiger = `${url}?pet_id:1111`;
+    mockReq(tiger)
+      .then(response => response.class.should.equal("mammal"))
       .then(response => response.family.should.equal("Felidae"))
-      .catch(err => console.error(err));
+      .then(response => response.species.should.equal("Tiger"))
+      .then(response => response.should.have.property("img_url"));
   });
 });
